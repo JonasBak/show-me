4769b0fc8df8c774ef0c2214d9532135f01d08a0
Jonas Bakken <jonasxb@gmail.com>
2019-05-25 01:06:08 +0200

    Fix old features in go

--- server.go
+++ server.go
@@ -1,25 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "os/exec"
+  "fmt"
+  "net/http"
+  "os/exec"
+  "hash/fnv"
+  "strings"
 )
 
+
+type file struct {
+  hash uint32
+  content string
+}
+
 var (
-	file string
+	loaded file
 )
 
 func main() {
-  file = "yeet"
+  loaded = file {0, "No file loaded yet"}
   http.HandleFunc("/", HelloServer)
+  http.HandleFunc("/ts", TsServer)
   http.ListenAndServe(":5555", nil)
+}
+
+func hash(s string) uint32 {
+  h := fnv.New32a()
+  h.Write([]byte(s))
+  return h.Sum32()
+}
+
+func TsServer(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, fmt.Sprint(loaded.hash))
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
   if r.Method == http.MethodGet {
-    fmt.Fprintf(w, file)
+    fmt.Fprintf(w, loaded.content)
   } else if r.Method == http.MethodPost {
     args := []string{"-t", "html", "-H", "reload.html"}
 
@@ -31,20 +49,12 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
     cmd := exec.Command("pandoc", args...)
     cmd.Stdin = r.Body
 
-    stdout, err := cmd.StdoutPipe()
+    b, err := cmd.Output()
     if err != nil {
       panic(err)
     }
-    go func() {
-      b, err := ioutil.ReadAll(stdout)
-      if err != nil {
-        panic(err)
-      }
-      // TODO replace hash
-      file = string(b)
-    }()
-
-    cmd.Start()
-    cmd.Wait()
+    content := string(b)
+    ts := hash(content)
+    loaded = file {ts, strings.Replace(content, "/*hash*/", fmt.Sprintf("\"%d\"", ts), 1)}
   }
 }
